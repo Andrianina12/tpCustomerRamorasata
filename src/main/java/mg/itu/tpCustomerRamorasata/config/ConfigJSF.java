fbceb56c2e2a839b2277e39c7ebddb449efd534f
Andrianina12 <ramorasata.andrianina@gmail.com>
2023-03-15 23:57:42 +0300

    Front end

---
+++ src/main/java/mg/itu/tpCustomerRamorasata/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpCustomerRamorasata.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author 26134
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
